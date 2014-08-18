f6d1351968504221438f07e0f7b28e887812a2ee
Stephen L. McMahon <stephen@slmcmahon.com>
2014-08-18 08:14:08 -1000

    Initial Commit

---
+++ Quakes2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Quakes2
+//
+//  Created by Stephen L. McMahon on 8/18/14.
+//  Copyright (c) 2014 Stephen L. McMahon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quakes2Tests/Quakes2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Quakes2Tests.m
+//  Quakes2Tests
+//
+//  Created by Stephen L. McMahon on 8/18/14.
+//  Copyright (c) 2014 Stephen L. McMahon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Quakes2Tests : XCTestCase
+
+@end
+
+@implementation Quakes2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
