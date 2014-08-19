5578a044b96bc1a43b21e4ad5618779f992f3f4b
Stephen L. McMahon <stephen@slmcmahon.com>
2014-08-18 18:53:07 -1000

    checkpoint

--- Quakes2/QuakesTableViewModel.m
+++ Quakes2/QuakesTableViewModel.m
@@ -8,12 +8,13 @@
 
 #import "QuakesTableViewModel.h"
 #import "QuakeDetail.h"
+#import "AFNetworking.h"
 
 @implementation QuakesTableViewModel
 
 -(id)init {
     if (self = [super init]) {
-        [self initQuakes];
+        [self loadQuakeData];
     }
     
     return self;
@@ -25,6 +26,31 @@
     QuakeDetail *q3 = [[QuakeDetail alloc] initWithHeader:@"Quake 3" magnitude:7.1 andTsunami:NO];
     
     _quakes = [NSArray arrayWithObjects:q1, q2, q3, nil];
+}
+
+- (void)loadQuakeData {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    [manager GET:@"http://earthquake.usgs.gov/earthquakes/feed/v0.1/summary/significant_month.geojson" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSDictionary *data = (NSDictionary *)responseObject;
+        NSArray *features = data[@"features"];
+        NSMutableArray *tmp = [[NSMutableArray alloc] init];
+        for (NSDictionary *feature in features) {
+            NSDictionary *props = feature[@"properties"];
+            NSString *place = props[@"place"];
+            float magnitude = [props[@"mag"] floatValue];
+            id tsunamiObject = props[@"tsunami"];
+            BOOL tsunami = NO;
+            if (tsunamiObject != [NSNull null]) {
+                tsunami = [tsunamiObject boolValue];
+            }
+            
+            QuakeDetail *q = [[QuakeDetail alloc] initWithHeader:place magnitude:magnitude andTsunami:tsunami];
+            [tmp addObject:q];
+        }
+        _quakes = tmp;
+        
+    } failure:nil];
 }
 
 @end
